325681724b5584135fbbf144818125a4f38a4f5c
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2025-10-20 08:57:39 -0300

    Add pet container component and update layout
    Introduces a new PetContainer component with associated template and styles, and sets it as the default route. Updates the main app layout for improved structure and styling, adds image assets, and enhances the registration form with a name field and updated button text.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Register } from './register/register';
+import { PetContainer } from './pet-container/pet-container';
 
-export const routes: Routes = [{
-    path: "login",
-    component: Login
-}, {
-    path: "register",
-    component: Register
-}];
+export const routes: Routes = [
+    {
+        path: "",
+        component: PetContainer
+    }, {
+        path: "login",
+        component: Login
+    }, {
+        path: "register",
+        component: Register
+    }];
---
+++ src/app/pet-container/pet-container.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-container',
+  imports: [],
+  templateUrl: './pet-container.html',
+  styleUrl: './pet-container.css'
+})
+export class PetContainer {
+
+}
